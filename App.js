06d87a61497c6e9dc32592eca2c91a5045180fca
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2023-04-07 19:14:33 +0300

    navigation and screen skeletons

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/screens/Home';
+import Newest from './src/screens/Newest';
+import Profile from './src/screens/Profile';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Newest') {
+            iconName = focused ? 'newspaper' : 'newspaper-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'ios-person-circle' : 'ios-person-outline';
+          } 
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {backgroundColor: '#182647'}
+        
+      })}>
+        <Tab.Screen name="Home" component={Home} options={{headerStyle: {backgroundColor: '#182647'}, headerTintColor: '#fff', headerTitleStyle: {fontFamily: 'Palatino-Bold'}}}/>
+        <Tab.Screen name="Newest" component={Newest} />
+        <Tab.Screen name="Profile" component={Profile}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#053096',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/screens/Newest.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Newest() {
+    return(
+        <View>
+            <Text>news feed</Text>
+        </View>
+    );
+}
