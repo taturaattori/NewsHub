0ed5c76c8060eb48b21118294ebb0f60261435dd
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2023-04-09 20:59:14 +0300

    testing apikey and fetch

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { API_KEY } from "@env"
 
 export default function Home() {
+    const [newsData, setNewsData] = useState([]);
+
+    const fetchNews = () => {
+        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setNewsData(data.articles))
+        .catch(error => console.error(error));
+    }
+
+    useEffect(() => {fetchNews()}, [])
+
     return(
         <View style={styles.container}>
-            <Text style={{color: 'white'}}>Home screen</Text>
+            {newsData.map(article => (
+                <View key={article.url}>
+                    <Text>{article.title}</Text>
+                    <Text>{article.description}</Text>
+                </View>
+            ))}
         </View>
     );
 }
