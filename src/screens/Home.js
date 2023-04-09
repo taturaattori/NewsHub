115d7818820ef7ba82cc474466e283dc5b2fddd7
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2023-04-09 22:08:57 +0300

    styled flatlist #1

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from "react-native";
 import { API_KEY } from "@env"
+import moment from "moment/moment";
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function Home() {
     const [newsData, setNewsData] = useState([]);
@@ -16,12 +18,31 @@ export default function Home() {
 
     return(
         <View style={styles.container}>
-            {newsData.map(article => (
-                <View key={article.url}>
-                    <Text>{article.title}</Text>
-                    <Text>{article.description}</Text>
-                </View>
-            ))}
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={newsData}
+                renderItem={({ item }) => (
+                <TouchableOpacity activeOpacity={1} style={styles.container}>
+                    <Image
+                        source={{
+                            uri: item?.urlToImage,
+                            cache: 'force-cache',
+                        }}
+                        resizeMode={'cover'}
+                        style={news.image}
+                    />
+                    <LinearGradient
+                        colors={['#0000', '#000A', '#000']}
+                        style={news.titleContainer}>
+                        <Text style={news.text}>{item?.title}</Text>
+                        <Text style={news.timestamp}>
+                            {moment(item.publishedAt).format('HH:MM DD, MMMM')}
+                        </Text>
+                    </LinearGradient>
+                </TouchableOpacity>
+            )}
+            style={styles.list}
+            />
         </View>
     );
 }
@@ -29,8 +50,55 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#182647',
-      alignItems: 'center',
-      justifyContent: 'center',
+      flexGrow: 1,
+      paddingTop: 20
+    },
+    list: {
+        flex: 1,
+        flexGrow: 1,
+        paddingVertical: 8,
+      },
+  });
+
+const news = StyleSheet.create({
+    container: {
+      height: 240,
+      marginBottom: 18,
+      backgroundColor: '#eee',
+      borderRadius: 24,
+      marginHorizontal: 16,
+    },
+    imageContainer: {flex: 1},
+    image: {
+      flex: 1,
+      borderRadius: 24,
+      height: 300,
+    },
+    titleContainer: {
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      borderBottomLeftRadius: 24,
+      borderBottomRightRadius: 24,
+      height: 160,
+      paddingLeft: 16,
+      paddingRight: 10,
+      justifyContent: 'flex-end',
+      alignItems: 'flex-start',
+    },
+    text: {
+      fontSize: 18,
+      fontWeight: '600',
+      lineHeight: 24,
+      color: '#fff',
+      paddingBottom: 24,
+    },
+    timestamp: {
+      position: 'absolute',
+      color: '#eee',
+      fontSize: 12,
+      fontWeight: '300',
+      right: 16,
+      bottom: 8,
     },
   });
